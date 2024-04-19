1588dfe1897861c4383ff57045c38027621328c7
JULIANA RIOS DOS SANTOS <145798829+JURIOS1309@users.noreply.github.com>
2024-04-18 21:42:20 -0300

    Update 2A.sql

--- 2A.sql
+++ 2A.sql
@@ -1,3 +1,4 @@
-SELECT DISTINCT Código_de_barras
-FROM Produto
-WHERE Data_cadastro >= DATEADD(day, -10, GETDATE())
+SELECT DISTINCT p.Código_de_barras
+FROM Produto p
+INNER JOIN ProdutoInfo pi ON p.ID = pi.IDProduto
+WHERE pi.Data_Cadastro >= DATEADD(day, -10, GETDATE())
