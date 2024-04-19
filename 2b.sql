e092e419d9dc6df141f62c60b6802918e778e5b6
JULIANA RIOS DOS SANTOS <145798829+JURIOS1309@users.noreply.github.com>
2024-04-18 21:43:53 -0300

    Update 2b.sql

--- 2b.sql
+++ 2b.sql
@@ -1,5 +1,5 @@
-SELECT DISTINCT o.Nome
-FROM Origem o
-INNER JOIN ProdutoInfo pi ON o.Id = pi.IDOrigem
-WHERE MONTH(pi.Data_Cadastro) = 3 AND YEAR(pi.Data_Cadastro) = 2020
-   OR MONTH(pi.Data_Atualizacao) = 3 AND YEAR(pi.Data_Atualizacao) = 2020;
+SELECT DISTINCT Origem.Nome
+FROM Origem
+INNER JOIN ProdutoInfo ON Origem.ID = ProdutoInfo.IDOrigem
+WHERE MONTH(ProdutoInfo.DataAtualizacao) = 3 AND YEAR(ProdutoInfo.DataAtualizacao) = 2020
+
